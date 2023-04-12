39ba0b085b6b4b160d8f541bc21a0c7d5edbd5dd
Murillo Nunes <murillonunes@ufu.br>
2023-04-11 22:13:36 -0300

    links
    Links dos projetos

--- pages/twitch.jsx
+++ pages/twitch.jsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import React from 'react';
 import twitchImg from '../public/assets/projects/twitch.jpg';
 import { RiRadioButtonFill } from 'react-icons/ri';
-import Link from 'next/link';
 
 const twitch = () => {
   return (
@@ -17,7 +16,7 @@ const twitch = () => {
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>Twitch UI</h2>
+          <h2 className='py-2'>E-commerce</h2>
           <h3>Next JS / Tailwind / Next Auth</h3>
         </div>
       </div>
@@ -27,33 +26,27 @@ const twitch = () => {
           <p>Project</p>
           <h2>Overview</h2>
           <p>
-            This appplication was built using NEXT.JS and is styled with
-            Tailwind CSS. The application is hosted statically using Vercel.
-            This is a mobile responsive recreation of Twitch.tv and features
-            Next/Auth.js for authentication. Users may choose to be
-            authenticated with either a Github account or a Google account. A
-            few features to note with this project are lazy loading images using
-            the Image component, built in routing, and Next/Auth Context.
+            Ipsum Loren
           </p>
           <a
-            href='https://twitch-nextjs-tailwind.vercel.app/'
+            href='#'
             target='_blank'
             rel='noreferrer'
           >
             <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
           </a>
           <a
-            href='https://github.com/fireclint/twitch-nextjs-tailwind'
+            href='#'
             target='_blank'
             rel='noreferrer'
           >
-            <button className='px-8 py-2 mt-4'>Code</button>
+            <button className='px-8 py-2 mt-4'>Código</button>
           </a>
 
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
           <div className='p-2'>
-            <p className='text-center font-bold pb-2'>Technologies</p>
+            <p className='text-center font-bold pb-2'>Tecnologias</p>
             <div className='grid grid-cols-3 md:grid-cols-1'>
               <p className='text-gray-600 py-2 flex items-center'>
                 <RiRadioButtonFill className='pr-1' /> Next.JS

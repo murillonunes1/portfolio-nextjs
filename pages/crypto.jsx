972cdcf68ea48a477fc4aa6df33cae056e06cc6a
Murillo Nunes <murillonunes@ufu.br>
2023-04-11 22:15:26 -0300

    Links2
    Update links

--- pages/crypto.jsx
+++ pages/crypto.jsx
@@ -17,7 +17,7 @@ const crypto = () => {
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>Crypto App</h2>
+          <h2 className='py-2'>Plataforma de stream</h2>
           <h3>React JS / Tailwind / Firebase</h3>
         </div>
       </div>
@@ -29,23 +29,17 @@ const crypto = () => {
           <p>
             I built this app to demonstrate knowledge working in React JS and
             accessing API endpoints. This app was built using React JS and is
-            hosted on Firebase. This application supports user authentication
-            using firebase. Users are able to create a new account using their
-            email address and can then sign in to save coins to a user specific
-            list. This is made possble by creating a firestore cloud storage
-            database at the time of user registration. Another feature of this
-            application is dynamic routing through the &quot;React Router DOM&quot;
-            package. This application was built using the Coin Gecko API.
+            hosted on Firebase.
           </p>
           <a
-            href='https://github.com/fireclint/crypto-react-firebase'
+            href='#'
             target='_blank'
             rel='noreferrer'
           >
-            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+            <button className='px-8 py-2 mt-4 mr-8'>Código</button>
           </a>
           <a
-            href='https://cryptobase-yt.web.app/'
+            href='#'
             target='_blank'
             rel='noreferrer'
           >

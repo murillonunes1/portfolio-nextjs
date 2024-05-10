bbafcc480820b5386e739e1a64dedfad431ae1fc
Murillo Nunes <123185990+murillonunes1@users.noreply.github.com>
2024-05-10 14:17:48 -0300

    Update rentacar.jsx

--- pages/rentacar.jsx
+++ pages/rentacar.jsx
@@ -49,13 +49,13 @@ const crypto = () => {
             <p className='text-center font-bold pb-2'>Tecnologias</p>
             <div className='grid grid-cols-3 md:grid-cols-1 '>
               <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> JavaScript
+                <RiRadioButtonFill className='pr-1' /> React
               </p>
               <p className='text-gray-600 py-2 flex items-center'>
                 <RiRadioButtonFill className='pr-1' /> HTML5
               </p>
               <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> CSS
+                <RiRadioButtonFill className='pr-1' /> Tailwind
               </p>
             </div>
           </div>
